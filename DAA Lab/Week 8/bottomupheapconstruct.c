b00c442090c2b89fc0ff18205e722f2e270cba32
aiqqia <aiqqia.ag@gmail.com>
2021-06-07 14:21:09 +0530

    Updated Daa Lab 7 file

---
+++ DAA Lab/Week 8/bottomupheapconstruct.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+void heapify(int *h, int n){
+    int i,j,k,v;
+    bool heap;
+    for(i=(n/2);i>=1;i--){
+        k=i;
+        v=h[k];
+        heap = false;
+        while(!heap && (2+k) <= n){
+            j=2*k;
+            if(j<n)
+                if(h[j]<h[j+1])
+                    j=j+1;
+            if(v>=h[j])
+                heap=true;
+            else{
+                h[k]=h[j];
+                k=j;
+            }
+        }
+        h[k]=v;
+    }
+    return;
+}
+
+
+int main(){
+    int n,x;
+    printf("\nEnter number of elements : ");
+    scanf("%d",&x);
+    n=x+1;
+    int *h = (int*)malloc((sizeof(int))*n);
+    //int h[n+1];
+    printf("\nEnter elements : ");
+    for(int i=1;i<n;i++){
+        scanf("%d",&h[i]);
+    }
+    // printf("\n");
+    // for(int i=1;i<n;i++){
+    //     printf("%d",h[i]);
+    // }
+    heapify(h,n);
+    printf("\nThe heap created is : ");
+    for(int i=1;i<n;i++){
+        printf("%d ",h[i]);
+    }
+    printf("\n");
+    return 0;
+}
