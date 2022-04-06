948557f18273c1e7227b198e4c340cc82a9a25a5
aiqqia <aiqqia.ag@gmail.com>
2022-04-06 18:44:21 +0530

    Added changes to Midterm exam code

--- Parallel Computer Architecture and Programming Lab/midtermexam.c
+++ Parallel Computer Architecture and Programming Lab/midtermexam.c
@@ -19,23 +19,47 @@ int main(int argc, char* argv[]){
     printf("Process %d has received value: %d\n", rank, x);
     int rand = (50*(rank+1)+size);
     printf("Process %d has generated a new random value: %d\n", rank, rand);
-    int ran[4], farr[4];
-    MPI_Gather(&rand, 1, MPI_INT, ran, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    if(rank == 0){
-        printf("Random numbers generated are: ");
+
+    /// Following code works in case of Collective Communication but we were asked to do rest in Point to Point
+
+    // int ran[4], farr[4];
+    // MPI_Gather(&rand, 1, MPI_INT, ran, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // if(rank == 0){
+    //     printf("Random numbers generated are: ");
+    //     for(int i=0;i<size;i++){
+    //         printf("%d ", ran[i]);
+    //     }
+    //     printf("\n");
+    //     for(int i=0;i<size;i++){
+    //         farr[i] = ran[i] + arr[arr[i]];
+    //     }
+    //     printf("Final Output Array is: ");
+    //     for(int i=0;i<size;i++){
+    //         printf("%d ", farr[i]);
+    //     }
+    //     printf("\n");
+    // }
+
+    /// Using Point to Point Communication
+
+    int ran[4], r;
+
+    if(rank != 0){
+        MPI_Send(&rand, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+    }
+    else{
+        ran[0] = rand;
+        for(int i=1;i<size;i++){
+            MPI_Recv(&r, 1 , MPI_INT, rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            ran[rank] = r;
+        }
+        printf("Random numbers are: ");
         for(int i=0;i<size;i++){
             printf("%d ", ran[i]);
         }
-        printf("\n");
-        for(int i=0;i<size;i++){
-            farr[i] = ran[i] + arr[arr[i]];
-        }
-        printf("Final Output Array is: ");
-        for(int i=0;i<size;i++){
-            printf("%d ", farr[i]);
-        }
-        printf("\n");
     }
+
+
     MPI_Finalize();
     return 0;
 }
